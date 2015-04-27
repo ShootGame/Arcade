7a20bec0c5009b6b649e4143de997e0d9a9ad7f2
TheMolkaPL <themolkapl@gmail.com>
2015-04-27 16:54:33 +0200

    Użyjmy specjalnych klas dedykowanych dla implementacji Bukkit dla modułów, bibliotek oraz gier.

---
+++ src/main/java/pl/shg/arcade/bukkit/BukkitLibrary.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2015 TheMolkaPL - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Aleksander Jagiełło <themolkapl@gmail.com>, 2015
+ */
+package pl.shg.arcade.bukkit;
+
+import java.util.Date;
+import pl.shg.arcade.api.module.Library;
+
+/**
+ *
+ * @author Aleksander
+ */
+public abstract class BukkitLibrary extends Library implements BListener {
+    public BukkitLibrary(Date date, String id, String version) {
+        super(date, id, version);
+    }
+    
+    @Override
+    public void disable() {
+        Listeners.unregister(this);
+    }
+    
+    @Override
+    public void enable() {
+        Listeners.register(this);
+    }
+}
---
+++ src/main/java/pl/shg/arcade/bukkit/BukkitModule.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 TheMolkaPL - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Aleksander Jagiełło <themolkapl@gmail.com>, 2015
+ */
+package pl.shg.arcade.bukkit;
+
+import java.io.File;
+import java.util.Date;
+import pl.shg.arcade.api.map.ConfigurationException;
+import pl.shg.arcade.api.module.Module;
+
+/**
+ *
+ * @author Aleksander
+ */
+public abstract class BukkitModule extends Module implements BListener {
+    public BukkitModule(Date date, String id, String version) {
+        super(date, id, version);
+    }
+    
+    @Override
+    public void disable() {
+        Listeners.unregister(this);
+    }
+    
+    @Override
+    public void enable() {
+        Listeners.register(this);
+    }
+}
