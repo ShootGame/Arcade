8a80a76718fdb60b5d385f5dd76a95a6bf936100
TheMolkaPL <themolkapl@gmail.com>
2015-05-02 22:23:58 +0200

    Korzystamy ze wsparcia BungeeCord z pluginu ShootGame

--- src/main/java/pl/shg/arcade/bukkit/plugin/BungeeCordProxy.java
+++ src/main/java/pl/shg/arcade/bukkit/plugin/BungeeCordProxy.java
@@ -6,51 +6,28 @@
  */
 package pl.shg.arcade.bukkit.plugin;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Server;
-import pl.shg.arcade.api.Color;
 import pl.shg.arcade.api.human.Player;
 import pl.shg.arcade.api.server.ProxyServer;
 import pl.shg.arcade.api.util.Validate;
+import pl.shg.shootgame.api.server.Servers;
 
 /**
  *
  * @author Aleksander
  */
 public class BungeeCordProxy implements ProxyServer {
-    public static final String NAME = "BungeeCord";
-    
     @Override
     public void connect(Player player, String server) {
         Validate.notNull(player, "player can not be null");
         Validate.notNull(server, "server can not be null");
-        ByteArrayOutputStream array = new ByteArrayOutputStream();
-        DataOutputStream output = new DataOutputStream(array);
-        
-        player.sendMessage(Color.YELLOW + "Laczenie z serwerem " + Color.GOLD + server + Color.YELLOW + "...");
-        try {
-            output.writeUTF("Connect");
-            output.writeUTF(server.toLowerCase().replace(" ", "-"));
-        } catch (IOException ex) {
-            player.sendMessage(Color.RED + "Nastapil blad podczas laczenia z serwerem " + server + ": " + ex.getMessage());
-            Logger.getLogger(BungeeCordProxy.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        ((org.bukkit.entity.Player) player.getPlayer()).sendPluginMessage(
-                ArcadeBukkitPlugin.getPlugin(), this.getProxyName(), array.toByteArray());
+        Servers.getProxy().connect((org.bukkit.entity.Player) player.getPlayer(), Servers.getServer(server));
     }
     
     @Override
     public String getProxyName() {
-        return BungeeCordProxy.NAME;
+        return "BungeeCord";
     }
     
-    public static void register(Server server) {
-        Validate.notNull(server, "server can not be null");
-        server.getMessenger().registerOutgoingPluginChannel(ArcadeBukkitPlugin.getPlugin(), BungeeCordProxy.NAME);
-    }
+    public static void register(Server server) {}
 }
