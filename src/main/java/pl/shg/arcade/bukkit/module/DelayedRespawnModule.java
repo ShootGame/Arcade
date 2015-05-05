6fa367989a07780018bba6a89d288764dc4d5f17
TheMolkaPL <themolkapl@gmail.com>
2015-05-05 23:04:24 +0200

    Poprawne wykorzystanie PlayerRespawnMatchEvent w module delayed-respawn

--- src/main/java/pl/shg/arcade/bukkit/module/DelayedRespawnModule.java
+++ src/main/java/pl/shg/arcade/bukkit/module/DelayedRespawnModule.java
@@ -11,15 +11,16 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import org.bukkit.GameMode;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import pl.shg.arcade.api.Arcade;
+import pl.shg.arcade.api.event.Event;
+import pl.shg.arcade.api.event.EventListener;
+import pl.shg.arcade.api.event.PlayerRespawnMatchEvent;
 import pl.shg.arcade.api.human.Player;
 import pl.shg.arcade.api.map.ConfigurationException;
+import pl.shg.arcade.api.map.Spawn;
 import pl.shg.arcade.api.map.team.kit.KitType;
 import pl.shg.arcade.api.module.Module;
 import pl.shg.arcade.bukkit.BListener;
@@ -31,18 +32,24 @@ import pl.shg.arcade.bukkit.Listeners;
  * @author Aleksander
  */
 public class DelayedRespawnModule extends Module implements BListener {
+    public static int seconds = 10;
     private final Map<UUID, Long> dead = new HashMap<>();
-    private int seconds = 10;
+    private EventListener respawnListener;
     
     public DelayedRespawnModule() {
         super(new Date(2015, 4, 25), "delayed-respawn", "1.0");
     }
     
     @Override
-    public void disable() {}
+    public void disable() {
+        Event.unregisterListener(this.respawnListener);
+    }
     
     @Override
     public void enable() {
+        this.respawnListener = new PlayerRespawnMatch();
+        Event.registerListener(this.respawnListener);
+        
         Arcade.getServer().getScheduler().run(new Runnable() {
             private final DelayedRespawnModule module = (DelayedRespawnModule) Module.of(DelayedRespawnModule.class);
             private final int defaults = this.module.getSeconds() * 1000;
@@ -64,7 +71,7 @@ public class DelayedRespawnModule extends Module implements BListener {
     public void load(File file) throws ConfigurationException {
         Listeners.register(this);
         FileConfiguration config = Config.get(file);
-        this.seconds = Config.getValueInt(config, this, "seconds");
+        seconds = Config.getValueInt(config, this, "seconds");
     }
     
     @Override
@@ -82,32 +89,40 @@ public class DelayedRespawnModule extends Module implements BListener {
         this.dead.put(player.getUUID(), System.currentTimeMillis());
     }
     
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerRespawn(PlayerRespawnEvent e) {
-        Player player = Arcade.getServer().getPlayer(e.getPlayer().getUniqueId());
-        if (player.isObserver()) {
-            return;
-        }
-        
-        player.sendMessage("Zostaniesz odrodzony/a za " + this.seconds + " sekund...");
-        this.dead(player, e.getPlayer());
-    }
-    
-    public void dead(Player player, org.bukkit.entity.Player bukkit) {
-        bukkit.setGameMode(GameMode.CREATIVE);
-    }
-    
     public Map<UUID, Long> getDead() {
         return this.dead;
     }
     
     public int getSeconds() {
-        return this.seconds;
+        return seconds;
     }
     
     public void respawn(UUID uuid) {
         Player player = Arcade.getServer().getPlayer(uuid);
         Arcade.getPlayerManagement().setAsPlayer(player, KitType.RESPAWN, false, false);
-        player.sendMessage("Zostales/as odrodzony/a po " + this.seconds + " sekundach.");
+        player.sendMessage("Zostales/as odrodzony/a po " + seconds + " sekundach.");
+    }
+    
+    private class PlayerRespawnMatch implements EventListener {
+        @Override
+        public Class<? extends Event> getEvent() {
+            return PlayerRespawnMatchEvent.class;
+        }
+        
+        @Override
+        public void handle(Event event) {
+            PlayerRespawnMatchEvent e = (PlayerRespawnMatchEvent) event;
+            if (!e.getPlayer().isObserver()) {
+                e.setCancel(true); // cancel this event, so play will not give kits, etc...
+                e.setSpawn((Spawn) e.getPlayer().getLocation()); // respawn player in the death location
+                
+                e.getPlayer().sendMessage("Zostaniesz odrodzony/a za " + DelayedRespawnModule.seconds + " sekund...");
+                this.dead(e.getPlayer());
+            }
+        }
+        
+        private void dead(Player player) {
+            
+        }
     }
 }
